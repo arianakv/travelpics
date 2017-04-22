5e02a2cd008dcc1a090a4b0a007e55ca4a5ae1e8
arianakv <ariana.viale@gmail.com>
2017-04-21 21:02:10 -0400

    images

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,12 +1,25 @@
 class PicturesController < ApplicationController
   def index
+   @pictures = Picture.all
   end
   def show
   end
   def create
+    @picture = Picture.create(picture_params)
+      redirect_to pictures_path
   end
   def delete
   end
   def update
   end
+    def new
+  @picture = Picture.new
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:image, :caption)
+  end
+
 end
